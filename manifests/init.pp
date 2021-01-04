e04f500b2f21fea412d846454a5f408ee8ab7552
Benjamin Robertson <benrobertson9876@gmail.com>
2021-01-05 10:31:14 +1100

    install DB

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,14 +7,19 @@
 class observium {
 
 # Check what OS we are on
-if $facts['os']['family'] == 'RedHat' {
-  include observium::yum
-}
+  if $facts['os']['family'] == 'RedHat' {
+    include observium::yum
+  }
 
 # install required packages
-$required_packages = lookup('observium::required_packages', Array)
-package { $required_packages:
-  ensure => 'latest',
-}
+  $required_packages = lookup('observium::required_packages', Array)
+  package { $required_packages:
+    ensure => 'latest',
+  }
 
+# Setup mariadb
+include observium::mariadb
+
+
+Class['observium::yum'] -> Class['observium::mariadb']
 }
---
+++ manifests/mariadb.pp
@@ -0,0 +1,10 @@
+# Class: observium
+#
+class observium::mariadb {
+  Class { '::mysql::server':
+    package_name   => 'mariadb-server',
+    package_ensure => 'present',
+    service_name   => 'mariadb',
+    root_password  => 'securelater',
+  }
+}
