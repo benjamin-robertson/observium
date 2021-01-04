9cd8656d1e9b41098758e08c6a031f1b5a60edf9
Benjamin Robertson <benrobertson9876@gmail.com>
2021-01-05 10:13:15 +1100

    Install packages

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,8 +7,14 @@
 class observium {
 
 # Check what OS we are on
-  if $facts['os']['family'] == 'RedHat' {
-    include observium::yum
-  }
+if $facts['os']['family'] == 'RedHat' {
+  include observium::yum
+}
+
+# install required packages
+$required_packages = lookup('observium::required_packages', Array)
+package { $required_packages:
+  ensure => 'latest',
+}
 
 }
