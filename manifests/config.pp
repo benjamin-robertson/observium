7bee7229bf111da84b10e8b4dbc0907e3a33cd24
Benjamin Robertson <benrobertson9876@gmail.com>
2021-01-05 13:52:34 +1100

    fix

--- manifests/config.pp
+++ manifests/config.pp
@@ -6,7 +6,7 @@ class observium::config inherits observium {
   # Setup config.php
   file { '/opt/observium/config.php':
     ensure  => file,
-    content => template('observium/config.epp', { 'db_host' => $db_host, 'db_user' => $db_user, 'db_password' => $db_password }),
+    content => epp('config.epp', { 'db_host' => $db_host, 'db_user' => $db_user, 'db_password' => $db_password }),
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
