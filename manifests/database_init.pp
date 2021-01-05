10d6cf94272268aa9a447c55b418df98331bc853
Benjamin Robertson <benrobertson9876@gmail.com>
2021-01-05 17:38:38 +1100

    fix

--- manifests/database_init.pp
+++ manifests/database_init.pp
@@ -5,7 +5,7 @@
 class observium::database_init inherits observium {
   # init the database if the user table is not present
   exec { '/opt/observium/discovery.php -u':
-    unless => "bin/mysql -u observium --password=${db_password} observium -e 'select * from users'"
+    unless => "/bin/mysql -u observium --password=${db_password} observium -e 'select * from users'"
   }
-  notify { "bin/mysql -u observium --password=${db_password} observium -e 'select * from users'": }
+  notify { "/bin/mysql -u observium --password=${db_password} observium -e 'select * from users'": }
 }
