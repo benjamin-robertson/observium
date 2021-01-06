9be965bce7986bab50b7c4c5dbd9d851f7459f02
Benjamin Robertson <benrobertson9876@gmail.com>
2021-01-06 13:10:56 +1100

    test

--- manifests/apache.pp
+++ manifests/apache.pp
@@ -16,7 +16,7 @@ class observium::apache inherits observium {
     access_log_file => '/opt/observium/logs/access_log',
     error_log_file  => '/opt/observium/logs/error_log',
     directories      => [
-      { 'path'  => '/opt/observium/html/',
+      { 'path'  => '/opt/observium/html1/',
         'suphp' => {
           user  => 'myappuser',
           group => 'myappgroup',
