e54a33079a7802981c1b82579fa95dc51cbd5d9b
Benjamin Robertson <benrobertson9876@gmail.com>
2021-01-07 16:46:28 +1100

    test new cron

--- manifests/install.pp
+++ manifests/install.pp
@@ -40,12 +40,18 @@ class observium::install {
   }
 
   # include cron to manage cron service
-  include cron
+  #include cron
   # include cron file for observium
-  file { '/etc/cron.d/observium':
-    ensure  => file,
-    mode    => '0644',
-    content => file('observium/cron.txt'),
-    notify  => Service['crond'],
+  #file { '/etc/cron.d/observium':
+  #  ensure  => file,
+  #  mode    => '0644',
+  #  content => file('observium/cron.txt'),
+  #  notify  => Service['crond'],
+  #}
+  cron { 'discovery all devices':
+    command => '/opt/observium/discovery.php -h all >> /dev/null 2>&1',
+    user    => 'root',
+    hour    => '*/6',
+    minute  => '33',
   }
 }
