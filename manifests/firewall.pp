9628c67b4680987fa8f4d6d62cb4f55edab79963
Benjamin Robertson <benrobertson9876@gmail.com>
2021-01-07 16:06:50 +1100

    updatefiorewall do

--- manifests/firewall.pp
+++ manifests/firewall.pp
@@ -3,11 +3,5 @@
 # Manages firewall and opens port for observium
 #
 class observium::firewall {
-  include firewall
-  #firewall { "Accept observium http traffic on port ${observium::apache_port}":
-  #  proto  => 'tcp',
-  #  action => 'accept',
-  #  dport  => $observium::apache_port,
-  #  zone   => 'public',
-  #}
+  
 }
