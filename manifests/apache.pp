0d1bc803a5790edd6ca8c6ba1dc55e4a0e2edcd2
Benjamin Robertson <benrobertson9876@gmail.com>
2021-01-08 14:00:15 +1100

    with ssl

--- manifests/apache.pp
+++ manifests/apache.pp
@@ -12,7 +12,29 @@ class observium::apache {
     }
   }
 
-# Specify virtual host
+# Specify virtual host - check if we are doing ssl or not
+if $manage_ssl {
+  # We are doing SSL
+  apache::vhost { $observium::apache_hostname:
+    port            => $observium::apache_sslport,
+    docroot         => '/opt/observium/html/',
+    servername      => $observium::apache_hostname,
+    access_log_file => '/opt/observium/logs/access_log',
+    error_log_file  => '/opt/observium/logs/error_log',
+    ssl             => true,
+    ssl_cert        => '/etc/ssl/observium_cert.pem',
+    ssl_key         => '/etc/ssl/observium_key.pem',
+    directories     => [
+      { 'path'           => '/opt/observium/html/',
+        'options'        => 'FollowSymLinks MultiViews',
+        'allow_override' => 'All',
+        'auth_require'   => 'all granted',
+      },
+    ],
+  }
+}
+else {
+  # We are not doing SSL
   apache::vhost { $observium::apache_hostname:
     port            => $observium::apache_port,
     docroot         => '/opt/observium/html/',
@@ -27,6 +49,8 @@ class observium::apache {
       },
     ],
   }
+}
+
 
 # Include php module
   if $observium::manage_apachephp {
