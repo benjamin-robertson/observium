78623d67c1b370ea7dc49a13002807dc8c8dac07
Benjamin Robertson <benrobertson9876@gmail.com>
2021-01-13 18:10:33 +1100

    manage php version with hiera

--- manifests/apache.pp
+++ manifests/apache.pp
@@ -5,7 +5,7 @@
 class observium::apache {
   assert_private()
 
-# Declare base apache class
+  # Declare base apache class
   if $observium::manage_apache {
     class { 'apache':
       default_vhost => false,
@@ -16,58 +16,52 @@ class observium::apache {
     }
   }
 
-# Specify virtual host - check if we are doing ssl or not
-if $observium::manage_ssl {
-  # We are doing SSL
-  apache::vhost { $observium::apache_hostname:
-    port            => $observium::apache_sslport,
-    docroot         => '/opt/observium/html/',
-    servername      => $observium::apache_hostname,
-    access_log_file => '/opt/observium/logs/access_log',
-    error_log_file  => '/opt/observium/logs/error_log',
-    ssl             => true,
-    ssl_cert        => $observium::custom_ssl_cert,
-    ssl_key         => $observium::custom_ssl_key,
-    directories     => [
-      { 'path'           => '/opt/observium/html/',
-        'options'        => 'FollowSymLinks MultiViews',
-        'allow_override' => 'All',
-        'auth_require'   => 'all granted',
-      },
-    ],
+  # Specify virtual host - check if we are doing ssl or not
+  if $observium::manage_ssl {
+    # We are doing SSL
+    apache::vhost { $observium::apache_hostname:
+      port            => $observium::apache_sslport,
+      docroot         => '/opt/observium/html/',
+      servername      => $observium::apache_hostname,
+      access_log_file => '/opt/observium/logs/access_log',
+      error_log_file  => '/opt/observium/logs/error_log',
+      ssl             => true,
+      ssl_cert        => $observium::custom_ssl_cert,
+      ssl_key         => $observium::custom_ssl_key,
+      directories     => [
+        { 'path'           => '/opt/observium/html/',
+          'options'        => 'FollowSymLinks MultiViews',
+          'allow_override' => 'All',
+          'auth_require'   => 'all granted',
+        },
+      ],
+    }
   }
-}
-else {
-  # We are not doing SSL
-  apache::vhost { $observium::apache_hostname:
-    port            => $observium::apache_port,
-    docroot         => '/opt/observium/html/',
-    servername      => $observium::apache_hostname,
-    access_log_file => '/opt/observium/logs/access_log',
-    error_log_file  => '/opt/observium/logs/error_log',
-    directories     => [
-      { 'path'           => '/opt/observium/html/',
-        'options'        => 'FollowSymLinks MultiViews',
-        'allow_override' => 'All',
-        'auth_require'   => 'all granted',
-      },
-    ],
+  else {
+    # We are not doing SSL
+    apache::vhost { $observium::apache_hostname:
+      port            => $observium::apache_port,
+      docroot         => '/opt/observium/html/',
+      servername      => $observium::apache_hostname,
+      access_log_file => '/opt/observium/logs/access_log',
+      error_log_file  => '/opt/observium/logs/error_log',
+      directories     => [
+        { 'path'           => '/opt/observium/html/',
+          'options'        => 'FollowSymLinks MultiViews',
+          'allow_override' => 'All',
+          'auth_require'   => 'all granted',
+        },
+      ],
+    }
   }
-}
 
 
-# Include php module
+  # Include php module - old 
+  $apache_php_version = lookup(observium::apache_php_version)
   if $observium::manage_apachephp {
-    case $facts['os']['family'] {
-      'Redhat': {
-        class { 'apache::mod::php':
-          php_version => '7',
-        }
-      }
-      'Debian': {
-        include apache::mod::php
-      }
-      default: { }
+    # New way
+    class { 'apache::mod::php':
+      php_version => $apache_php_version,
     }
   }
 }
