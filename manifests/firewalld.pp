0837c10a5f1cba3a543e0164cb055fc90433a551
Benjamin Robertson <benrobertson9876@gmail.com>
2021-01-14 11:37:37 +1100

    removed manage fw from firewall classes

--- manifests/firewalld.pp
+++ manifests/firewalld.pp
@@ -4,27 +4,25 @@
 #
 class observium::firewalld {
   assert_private()
-  # Check we are managing firewall
-  if observium::manage_fw {
-    class { 'firewalld': }
-    # Check if we are using ssl
-    if $observium::manage_ssl {
-      # We are doing the ssl thing :)
-      firewalld_port { "Open port ${observium::apache_sslport} for observium":
-        ensure   => present,
-        zone     => 'public',
-        port     => $observium::apache_sslport,
-        protocol => 'tcp',
-      }
+
+  class { 'firewalld': }
+  # Check if we are using ssl
+  if $observium::manage_ssl {
+    # We are doing the ssl thing :)
+    firewalld_port { "Open port ${observium::apache_sslport} for observium":
+      ensure   => present,
+      zone     => 'public',
+      port     => $observium::apache_sslport,
+      protocol => 'tcp',
     }
-    else {
-      # not doing the ssl thing :(
-      firewalld_port { "Open port ${observium::apache_port} for observium":
-        ensure   => present,
-        zone     => 'public',
-        port     => $observium::apache_port,
-        protocol => 'tcp',
-      }
+  }
+  else {
+    # not doing the ssl thing :(
+    firewalld_port { "Open port ${observium::apache_port} for observium":
+      ensure   => present,
+      zone     => 'public',
+      port     => $observium::apache_port,
+      protocol => 'tcp',
     }
   }
 }
--- manifests/firewallufw.pp
+++ manifests/firewallufw.pp
@@ -5,25 +5,23 @@
 class observium::firewallufw {
   assert_private()
 
-  if observium::manage_fw {
-    # Add rules for apache
-    class { 'ufw': }
-    if $observium::manage_ssl {
-      ufw::allow { "Allow https access ${observium::apache_sslport}":
-        port => $observium::apache_sslport,
-        from => '0.0.0.0/0',
-      }
-    }
-    else {
-      ufw::allow { "Allow https access ${observium::apache_port}":
-        port => $observium::apache_port,
-        from => '0.0.0.0/0',
-      }
-    }
-    # Ensure ssh is open
-    ufw::allow { 'Allow ssh access 22':
-      port => '22',
+  # Add rules for apache
+  class { 'ufw': }
+  if $observium::manage_ssl {
+    ufw::allow { "Allow https access ${observium::apache_sslport}":
+      port => $observium::apache_sslport,
       from => '0.0.0.0/0',
     }
+  }
+  else {
+    ufw::allow { "Allow https access ${observium::apache_port}":
+      port => $observium::apache_port,
+      from => '0.0.0.0/0',
+    }
+  }
+  # Ensure ssh is open
+  ufw::allow { 'Allow ssh access 22':
+    port => '22',
+    from => '0.0.0.0/0',
   }
 }
