88e4ec8d54de10b0b10cb5ede10bc8de2c9d818e
ben <benrobertson9876@gmail.com>
2024-05-22 13:54:46 +1000

    Push to github

---
+++ spec/acceptance/observium_install_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'spec_helper_acceptance'
+
+describe 'Installation', if: ['centos', 'redhat', 'ubuntu'].include?(os[:family]) do
+    let(:pp) do
+      <<-MANIFEST
+        include observium
+      MANIFEST
+    end
+  
+    it 'applies idempotently' do
+      idempotent_apply(pp)
+    end
+  
+    describe file("/opt/observium/config.php") do
+      it { is_expected.to be_file }
+    #   its(:content) { is_expected.to match %r{key = default value} }
+    end
+  
+    describe port(80) do
+      it { is_expected.to be_listening }
+    end
+  end
