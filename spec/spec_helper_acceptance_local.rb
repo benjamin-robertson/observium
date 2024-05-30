0bf04686873191566f339fcd56e6d4a8ab646c9b
ben <benrobertson9876@gmail.com>
2024-05-31 08:56:38 +1000

    remove do

--- spec/spec_helper_acceptance_local.rb
+++ spec/spec_helper_acceptance_local.rb
@@ -1,10 +1,10 @@
 include PuppetLitmus
 
 def install_packge(package)
-    if :osfamily == 'Ubuntu' do
+    if :osfamily == 'Ubuntu'
         run_shell("apt -y install #{package}")
     end
-    if :osfamily == 'RedHat' do
+    if :osfamily == 'RedHat'
         run_shell("yum -y install #{package}")
     end
     
