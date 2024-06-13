4e52059b35ed587a1776348bd30d2a3e7d91ac88
ben <benrobertson9876@gmail.com>
2024-06-13 14:27:30 +1000

    test unit tests

--- spec/classes/observium_spec.rb
+++ spec/classes/observium_spec.rb
@@ -9,11 +9,50 @@ describe 'observium' do
 
       it { is_expected.to compile }
 
-      if :operatingsystem == 'RedHat' 
-        it { is_expected.to contain_service('httpd') }
-      elsif :operatingsystem == 'Ubuntu'
-        it { is_expected.to contain_service('apache2') }
-      end
+
+      # it { is_expected.to contain_service('apache2') }
+      # it { is_expected.to contain_service(os) }
+
     end
+  end
+
+  context "on rhel7" do
+    let(:facts) do
+      { 
+        'os' => {
+          'family' => 'RedHat',
+          'release' => {
+            'major' => '7',
+          },
+          'selinux' => {
+            'enabled' => true,
+            'current_mode' => 'enforcing',
+          },
+        }
+      }
+    end
+    
+    it { is_expected.to contain_service('httpd') }
+  end
+
+  context "on rhel8" do
+    let(:facts) do
+      { 
+        'os' => {
+          'family' => 'RedHat',
+          'release' => {
+            'major' => '8',
+          },
+          'selinux' => {
+            'enabled' => true,
+            'current_mode' => 'enforcing',
+          },
+        }
+      }
+    end
+
+    it { is_expected.to contain_service('httpd') }
+    it { is_expected.to contain_package('python3-PyMySQL')}
+    it { is_expected.to contain_package('php-json')}
   end
 end
