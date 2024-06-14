90d94ef950edb943d6a562bd3c47e5fd76e54084
ben <benrobertson9876@gmail.com>
2024-06-14 14:20:59 +1000

    check os release

--- spec/acceptance/observium_install_spec.rb
+++ spec/acceptance/observium_install_spec.rb
@@ -40,6 +40,22 @@ describe 'Installation', if: ['centos', 'redhat', 'ubuntu'].include?(os[:family]
     it { should have_entry('33 */6 * * * /opt/observium/discovery.php -h all >> /dev/null 2>&1').with_user('root') }
   end
 
+  describe cron do
+    it { should have_entry('*/5 * * * * /opt/observium/discovery.php -h new >> /dev/null 2>&1').with_user('root') }
+  end
+
+  describe cron do
+    it { should have_entry('*/5 * * * * /opt/observium/poller-wrapper.py >> /dev/null 2>&1').with_user('root') }
+  end
+
+  describe cron do
+    it { should have_entry('5 13 * * * /opt/observium/housekeeping.php -ysel').with_user('root') }
+  end
+
+  describe cron do
+    it { should have_entry('47 4 * * * /opt/observium/housekeeping.php -yrptb').with_user('root') }
+  end
+
   # Red hat specifc checks
   if os[:family] == 'redhat'
 
@@ -53,10 +69,6 @@ describe 'Installation', if: ['centos', 'redhat', 'ubuntu'].include?(os[:family]
 
     describe package('python3-PyMySQL') do
       it { should be_installed }
-    end
-
-    describe selinux do
-      it { should be_permissive }
     end
 
     describe yumrepo('opennms-common') do
@@ -75,6 +87,10 @@ describe 'Installation', if: ['centos', 'redhat', 'ubuntu'].include?(os[:family]
 
     describe service('snmpd') do
       it { is_expected.to be_running }
+    end
+
+    describe package(os:[release]) do
+      it { should be_installed }
     end
 
   end
