b6a9e6337e6ea10197838aa2e25216547b602e39
ben <benrobertson9876@gmail.com>
2024-06-21 14:14:09 +1000

    fix

--- manifests/firewall.pp
+++ manifests/firewall.pp
@@ -15,22 +15,22 @@ class observium::firewall {
   #   proto => 'icmp',
   #   jump  => 'accept',
   # }
-  # -> firewall { '001 accept all to lo interface':
-  #   proto   => 'all',
-  #   iniface => 'lo',
-  #   jump    => 'accept',
-  # }
-  # -> firewall { '002 reject local traffic not on loopback interface':
-  #   iniface     => '! lo',
-  #   proto       => 'all',
-  #   destination => '127.0.0.1/8',
-  #   jump        => 'reject',
-  # }
-  # -> firewall { '003 accept related established rules':
-  #   proto => 'all',
-  #   state => ['RELATED', 'ESTABLISHED'],
-  #   jump  => 'accept',
-  # }
+  firewall { '001 accept all to lo interface':
+    proto   => 'all',
+    iniface => 'lo',
+    jump    => 'accept',
+  }
+  -> firewall { '002 reject local traffic not on loopback interface':
+    iniface     => '! lo',
+    proto       => 'all',
+    destination => '127.0.0.1/8',
+    jump        => 'reject',
+  }
+  -> firewall { '003 accept related established rules':
+    proto => 'all',
+    state => ['RELATED', 'ESTABLISHED'],
+    jump  => 'accept',
+  }
   # Add rules for apache
   if $observium::manage_ssl {
     firewall { "50 Allow https access ${observium::apache_sslport}":
