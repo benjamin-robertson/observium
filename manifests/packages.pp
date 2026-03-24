e3dede2eee8fc541b0461148cf566e43261dd981
Munir Nassar <nassarmu@umn.edu>
2026-03-24 16:22:28 -0500

    refactor ensure_packages

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -14,23 +14,17 @@ class observium::packages {
         '7': {
           # Running on rhel 7
           $required_packages = lookup('observium::required_packages', Array)
-          stdlib::ensure_packages(
-            $required_packages,
-            {
-              require => Class['observium::yum'],
-            }
-          )
+          $requirements = {
+            require => Class['observium::yum'],
+          }
         }
         '8': {
           # Running on rhel 8
           $required_packages = lookup('observium::required_packages', Array)
-          stdlib::ensure_packages(
-            $required_packages,
-            {
-              require => Class['observium::yum'],
-              before  => Exec['/sbin/alternatives --set python /usr/bin/python3'],
-            }
-          )
+          $requirements = {
+            require => Class['observium::yum'],
+            before  => Exec['/sbin/alternatives --set python /usr/bin/python3'],
+          }
 
           # Set python3 as /bin/python as observium expects this
           exec { '/sbin/alternatives --set python /usr/bin/python3':
@@ -40,20 +34,23 @@ class observium::packages {
         '9': {
           # Running on rhel 9
           $required_packages = lookup('observium::required_packages', Array)
-          stdlib::ensure_packages(
-            $required_packages,
-            {
-              require => Class['observium::yum'],
-            }
-          )
+          $requirements = {
+            require => Class['observium::yum'],
+          }
         }
         default: { fail('Unsupported operating system, bailing out!!') }
       }
     }
     'Debian': {
       $required_packages = lookup('observium::required_packages', Array)
-      stdlib::ensure_packages($required_packages)
+      $requirements = {}
     }
     default: { fail('Unsupported operating system, bailing out!!') }
   }
+
+  # install the required packages
+  stdlib::ensure_packages(
+    $required_packages,
+    $requirements,
+  )
 }
