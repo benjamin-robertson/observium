03be997e0919a80ee74a568fcadbe0f4e6c92ea2
Ben Robertson <benrobertson9876@gmail.com>
2026-04-05 14:40:59 +0000

    added extra tests

--- spec/acceptance/observium_install_spec.rb
+++ spec/acceptance/observium_install_spec.rb
@@ -53,7 +53,16 @@ describe 'Installation', if: ['centos', 'redhat', 'ubuntu'].include?(os[:family]
     it { is_expected.to be_listening }
   end
 
-  describe command("\/usr\/bin\/curl http:\/\/127.0.0.1 -I") do # for some reason this isn't working as expected. Disabling test.
+  describe command('ls /f') do
+    its(:exit_status) { should eq 0 }
+  end
+
+  describe command('which curl') do
+    its(:exit_status) { should eq 0 }
+    its(:stdout) { should match /\/usr\/bin\/curl/ }
+  end
+
+  describe command("/usr/bin/curl http://127.0.0.1 -I") do # for some reason this isn't working as expected. Disabling test.
     its(:exit_status) { is_expected.to eq 0 }
     its(:stdout) { is_expected.to contain "HTTP/1.1 200 OK" }
   end
